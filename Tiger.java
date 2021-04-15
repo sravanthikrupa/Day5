78a78b99ec071fcfdcd7377ed4936ae71a875ab9
sravanthikrupa <61538318+sravanthikrupa@users.noreply.github.com>
2021-04-15 14:26:35 +0530

    Add files via upload

---
+++ Tiger.java
@@ -0,0 +1,17 @@
+package Day5Task1;
+
+public class Tiger extends Animal {
+	public Tiger(int legs, float d) {
+		super(legs, d);
+	}
+	public void doPlay() {
+		System.out.println("Tiger Play method  called...");
+	}
+	public void doFeed(int food) {
+		super.weight += (food * 0.5);
+		System.out.println(" Inside dog implementation of feed method "+super.weight);
+	}
+	public void doRun() {
+		System.out.println(" Implementation of Dog Run method");
+	}
+}
